c6a7f5b14f82a47d297bb47f87865ffd3e6e292a
arielcessario <arielcessario@gmail.com>
2016-05-31 11:44:13 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -189,6 +189,17 @@
 
         // INIT
         ContactsService.facebookInit();
+        twitter(document,'script', 'twitter-wjs');
+        function twitter(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+            if (!d.getElementById(id)) {
+                js = d.createElement(s);
+                js.id = id;
+                js.src = p + '://platform.twitter.com/widgets.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+        };
+
 
         if (vm.user != false) {
             vm.isLogged = true;
